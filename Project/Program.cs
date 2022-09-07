ae5122557e384af81b7758f57510ccc06cf60fbf
Vadim Bayramov <bayramovva@gmail.com>
2022-09-08 01:04:47 +0300

    Add Code

---
+++ Project/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void ControlWork()
+        {
+            
+            string[] array = {"Три", ":)", "+", "Kazan", "Семь", "Архитектор", "NY"};
+            Console.Write("[");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    Console.Write(array[i] + ", ");
+                }
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+        
+        ControlWork();
+    }
+}
